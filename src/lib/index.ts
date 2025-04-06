80b872f24fed91da967226e1942efad1acc300de
Dinis <dinisrosas@hotmail.com>
2025-04-07 00:36:27 +0400

    prisma build

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,7 @@
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
-// import { PrismaClient } from '@prisma/client/edge';
-// import { withAccelerate } from '@prisma/extension-accelerate';
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
+export const prisma = new PrismaClient().$extends(withAccelerate());
