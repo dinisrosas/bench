0f82ff684d54b17e22a2e2c8a8c1efe6d7395eb1
Dinis <dinisrosas@hotmail.com>
2025-04-07 01:55:42 +0400

    partytown cookieyes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { partytownVite } from '@qwik.dev/partytown/utils';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		partytownVite({
+			dest: path.join(__dirname, 'build', '~partytown')
+		})
+	]
 });
