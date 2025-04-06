8d491c2e3114d225de03f3a81b40cf7832fa3ea6
Dinis <dinisrosas@hotmail.com>
2025-04-07 00:26:33 +0400

    fix

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
 import { createClient } from '@supabase/supabase-js';
 
