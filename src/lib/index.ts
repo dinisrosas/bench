df67c7ed494a8c6a96e33942fa8d1bca4fcf06a0
Dinis <dinisrosas@hotmail.com>
2025-04-07 00:29:06 +0400

    remove prisma

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,6 @@
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
-import { PrismaClient } from '@prisma/client/edge';
-import { withAccelerate } from '@prisma/extension-accelerate';
+// import { PrismaClient } from '@prisma/client/edge';
+// import { withAccelerate } from '@prisma/extension-accelerate';
 import { createClient } from '@supabase/supabase-js';
 
-export const prisma = new PrismaClient().$extends(withAccelerate())
 export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
--- src/routes/prisma/+page.server.ts
+++ src/routes/prisma/+page.server.ts
@@ -1,19 +1,19 @@
-import { prisma } from '$lib';
+// import { prisma } from '$lib';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
-	const products = await prisma.product.findMany({
-		omit: {
-			price: true
-		},
-		cacheStrategy: {
-			swr: 60,
-			ttl: 60,
-			tags: ['products']
-		}
-	});
+	// const products = await prisma.product.findMany({
+	// 	omit: {
+	// 		price: true
+	// 	},
+	// 	cacheStrategy: {
+	// 		swr: 60,
+	// 		ttl: 60,
+	// 		tags: ['products']
+	// 	}
+	// });
 
 	return {
-		products
+		products: []
 	};
 };
