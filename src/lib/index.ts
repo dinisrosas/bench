d2ea21a2414dd27015ad9a31fa588fe7c4c84a5f
Dinis <dinisrosas@hotmail.com>
2025-04-07 01:00:50 +0400

    add scripts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
-import { PrismaClient } from '@prisma/client/edge';
+import { PrismaClient } from '@prisma/client';
 import { withAccelerate } from '@prisma/extension-accelerate';
 import { createClient } from '@supabase/supabase-js';
 
